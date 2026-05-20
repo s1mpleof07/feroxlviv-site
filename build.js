89389153e0de40a1bb0a154ebd749eff0723a4c8
s1mpleof07 <prokopiv.andriy99@gmail.com>
2026-05-20 23:57:07 +0300

    Add files via upload

--- build.js
+++ build.js
@@ -82,8 +82,14 @@ function head(title, desc, keywords, canonical) {
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/styles.css">
 <script type="application/ld+json">{"@context":"https://schema.org","@type":"LocalBusiness","name":"FEROX LVIV","description":"Виготовлення дизайн-об'єктів з кортену та послуги металообробки у Львові","address":{"@type":"PostalAddress","addressLocality":"Львів","addressCountry":"UA"},"url":"https://feroxlviv.com.ua","serviceType":["Лазерна різка металу","Гнуття металу","Зварювання","Дизайн з кортену"]}</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KR9LWFWB');</script>
+<!-- End Google Tag Manager -->
 </head>
-<body>`;
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR9LWFWB" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`;
 }
 
 function nav(active = '') {
